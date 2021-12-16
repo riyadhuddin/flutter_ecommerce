2aef6df7d13c80555c51676cac22537c0cd4ccd3
Riyadh Uddin <noreplythnx@gmail.com>
2021-12-16 19:41:07 +0600

    addd

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:graphql/client.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+
 const productsGraphql = """
-query products(first: 10) {
+query products (\$first: 10, channel: "default-channel") {
     edges {
       node {
         id
@@ -16,10 +17,15 @@ query products(first: 10) {
   }
 """;
 void main() {
-  final HttpLink httplink = HttpLink('https://demo.saleor.io/graphql/');
-  
-  ValueNotifier<GraphQLClient> client = ValueNotifier(  GraphQLClient(link: httplink,cache: GraphQLCache(store: InMemoryStore()),),);
- var app = GraphQLProvider(client: client, child: MyApp());
+  final HttpLink httplink = HttpLink("https://demo.saleor.io/graphql/");
+
+  ValueNotifier<GraphQLClient> client = ValueNotifier(
+    GraphQLClient(
+      link: httplink,
+      cache: GraphQLCache(store: InMemoryStore()),
+    ),
+  );
+  var app = GraphQLProvider(client: client, child: MyApp());
   runApp(app);
 }
 
@@ -29,7 +35,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-    
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -40,7 +45,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-
   final String title;
 
   @override
@@ -48,31 +52,67 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
- 
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
         title: Text(widget.title),
       ),
-      body: Query(options: QueryOptions(document: gql(productsGraphql)),
-       builder: (QueryResult result, {fetchMore, refetch}){
-         if(result.hasException){
-           return Text(result.exception.toString());
-         }
-         if(result.isLoading){
-           return Center(child: CircularProgressIndicator(),);
-         }
-         final productList = result.data?['products']['edges'];
-        //  print(productList);
-        //  return Text("some");
-        return column(
-          
-        );
-       }),
+      body: Query(
+          options: QueryOptions(document: gql(productsGraphql)),
+          builder: (QueryResult result, {fetchMore, refetch}) {
+            if (result.hasException) {
+              return Text(result.exception.toString());
+            }
+            if (result.isLoading) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            final productList = result.data?['products']['edges'];
+            //  print(productList);
+            //  return Text("some");
+            return Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text(
+                    "Products",
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                ),
+                Expanded(
+                    child: GridView.builder(
+                        itemCount: productList.length,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            mainAxisSpacing: 2.0,
+                            crossAxisSpacing: 2.0,
+                            childAspectRatio: 0.75),
+                        itemBuilder: (_, index) {
+                          var product = productList[index]['node'];
+                          return Column(
+                            children: [
+                              Container(
+                                padding: EdgeInsets.all(2.0),
+                                width: 180,
+                                height: 180,
+                                child:
+                                    Image.network(product['thumbnail']['url']),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.symmetric(vertical: 8.0),
+                                child: Text(
+                                  product['name'],
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ],
+                          );
+                        }))
+              ],
+            );
+          }),
     );
   }
 }
